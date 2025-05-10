b10a8971b24bd7a4435f694e30ab1901d0c8f778
turjuman-dev <geben.tk@gmail.com>
2025-05-10 12:54:13 +0100

    س

--- netlify/functions/translate.js
+++ netlify/functions/translate.js
@@ -1,21 +1,30 @@
-const translate = require('google-translate-api');
+const translate = require('@vitalets/google-translate-api');
 
 exports.handler = async (event) => {
-  const { q, source = 'auto', target = 'en' } = JSON.parse(event.body);
+  // Allow CORS
+  const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type'
+  };
+
+  // Handle preflight
+  if (event.httpMethod === 'OPTIONS') {
+    return { statusCode: 200, headers };
+  }
+
   try {
+    const { q, source = 'auto', target = 'en' } = JSON.parse(event.body);
     const res = await translate(q, { from: source, to: target });
     return {
       statusCode: 200,
-      headers: { 
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*' 
-      },
+      headers,
       body: JSON.stringify({ translatedText: res.text })
     };
   } catch (err) {
     return {
       statusCode: 500,
-      headers: { 'Access-Control-Allow-Origin': '*' },
+      headers,
       body: JSON.stringify({ error: err.message })
     };
   }
